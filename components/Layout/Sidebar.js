e1dec448845bec9b03eff632004a153ca09a1929
Sahas <sahasvivek001@gmail.com>
2024-01-09 02:25:44 +0530

    fix: sidenav + topnav design changes

--- components/Layout/Sidebar.js
+++ components/Layout/Sidebar.js
@@ -2,12 +2,21 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 
-// import { SlHome } from 'react-icons/sl'
-// import { BsInfoSquare, BsEnvelopeAt } from 'react-icons/bs'
-// import { FaTshirt, FaRedhat } from 'react-icons/fa'
-
-// import logo from '@/img/logo.svg'
+import HomeIcon from '../../public/navicons/home-icon.svg';
+import OrdersIcon from '../../public/navicons/orders-icon.svg';
+import ProductsIcon from '../../public/navicons/products-icon.svg';
+import DeliveryIcon from '../../public/navicons/delivery-icon.svg';
+import MarketingIcon from '../../public/navicons/marketing-icon.svg';
+import AnalyticsIcon from '../../public/navicons/analytics-icon.svg';
+import PaymentsIcon from '../../public/navicons/payments-icon.svg';
+import ToolsIcon from '../../public/navicons/tools-icon.svg';
+import DiscountsIcon from '../../public/navicons/discounts-icon.svg';
+import AudienceIcon from '../../public/navicons/audience-icon.svg';
+import AppearanceIcon from '../../public/navicons/appearance-icon.svg';
+import PluginsIcon from '../../public/navicons/plugins-icon.svg';
+import DownArrowSideNav from '../../public/navicons/down-arrow-sidenav.svg';
 
 export default function Sidebar({ show, setter }) {
     const router = useRouter();
@@ -16,27 +25,6 @@ export default function Sidebar({ show, setter }) {
     const className = "transition-[margin-left] ease-in-out duration-500 fixed md:static top-0 bottom-0 left-0 z-40";
     // Append class based on state of sidebar visiblity
     const appendClass = show ? " ml-0" : " ml-[-250px] md:ml-0";
-
-    // Clickable menu items
-    const MenuItem = ({ icon, name, route }) => {
-        // Highlight menu item based on currently displayed route
-        const colorClass = router.pathname === route ? "text-white" : "text-white/50 hover:text-white";
-
-        return (
-            <Link
-                href={route}
-                onClick={() => {
-                    setter(oldVal => !oldVal);
-                }}
-                className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3 ${colorClass}`}
-            >
-                <div className="text-xl flex [&>*]:mx-auto w-[30px]">
-                    {icon}
-                </div>
-                <div className='text-white text-[14px] font-medium'>{name}</div>
-            </Link>
-        )
-    }
 
     // Overlay to prevent clicks in background, also serves as our close button
     const ModalOverlay = () => (
@@ -50,75 +38,124 @@ export default function Sidebar({ show, setter }) {
 
     return (
         <>
-            <div className={`${className}${appendClass} bg-[#1E2640] w-56`}>
-                <div className="p-2 flex">
-                    <Link href="/">
-                        <h2 className="text-white">Nishyan</h2>
-                        <p className="text-white underline">Visit Store!</p>
-                        {/* <img src={logo.src} alt="Company Logo" width={300} height={300} /> */}
-                    </Link>
+            <div className={`${className}${appendClass} bg-[#1E2640] w-64`}>
+                <div className="p-5 flex">
+                    <Image src="/dukaan-nav.png" alt="Company Logo" width={55} height={55} />
+                    <div className="flex flex-col pl-2 gap-[4px]">
+                        <h2 className="text-white font-medium text-[15px]">Nishyan</h2>
+                        <p className="text-white/80 underline font-normal text-[13px]">Visit Store!</p>
+                    </div>
+                    <Image src={DownArrowSideNav} alt="Down Arrow" width={18} height={18} className="ml-auto my-auto" />
                 </div>
                 <div className="flex flex-col">
-                    <MenuItem
-                        name="Home"
-                        route="/"
-                        // icon={<SlHome />}
-                    />
-                    <MenuItem
-                        name="Orders"
-                        route="/t-shirts"
-                        // icon={<FaTshirt />}
-                    />
-                    <MenuItem
-                        name="Products"
-                        route="/hats"
-                        // icon={<FaRedhat />}
-                    />
-                    <MenuItem
-                        name="Delivery"
-                        route="/about"
-                        // icon={<BsInfoSquare />}
-                    />
-                    <MenuItem
-                        name="Marketing"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Analytics"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Payments"
-                        route="/"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Tools"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Discounts"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Audience"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Appearance"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
-                    <MenuItem
-                        name="Plugins"
-                        route="/contact"
-                        // icon={<BsEnvelopeAt />}
-                    />
+                    <Link
+                        href="/"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={HomeIcon} alt="Home Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Home</div>
+                    </Link>
+                    <Link
+                        href="/orders"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={OrdersIcon} alt="Orders Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Orders</div>
+                    </Link>
+                    <Link
+                        href="/products"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={ProductsIcon} alt="Products Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Products</div>
+                    </Link>
+                    <Link
+                        href="/delivery"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={DeliveryIcon} alt="Delivery Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Delivery</div>
+                    </Link>
+                    <Link
+                        href="/marketing"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={MarketingIcon} alt="Marketing Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Marketing</div>
+                    </Link>
+                    <Link
+                        href="/analytics"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={AnalyticsIcon} alt="Analytics Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Analytics</div>
+                    </Link>
+                    <Link
+                        href="/payments"
+                        className={`flex [&>*]:my-auto text-md pl-4 py-3 bg-[#353C53] mx-2 rounded`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={PaymentsIcon} alt="Payments Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium px-1'>Payments</div>
+                    </Link>
+                    <Link
+                        href="/tools"
+                        className={`flex [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={ToolsIcon} alt="Tools Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium pl-[5px]'>Tools</div>
+                    </Link>
+                    <Link
+                        href="/discounts"
+                        className={`flex [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={DiscountsIcon} alt="Discounts Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium pl-[5px]'>Discounts</div>
+                    </Link>
+                    <Link
+                        href="/audience"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={AudienceIcon} alt="Audience Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Audience</div>
+                    </Link>
+                    <Link
+                        href="/appearance"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={AppearanceIcon} alt="Appearance Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Appearance</div>
+                    </Link>
+                    <Link
+                        href="/plugins"
+                        className={`flex gap-1 [&>*]:my-auto text-md pl-6 py-3`}
+                    >
+                        <div className="text-xl flex [&>*]:mx-auto w-[30px]">
+                            <Image src={PluginsIcon} alt="Plugins Icon" width={18} height={18} />
+                        </div>
+                        <div className='text-white/80 text-[14px] font-medium'>Plugins</div>
+                    </Link>
                 </div>
             </div>
             {show ? <ModalOverlay /> : <></>}
--- components/TopNav.js
+++ components/TopNav.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import HelpIcon from '../public/navicons/Help.svg';
+import SearchIcon from '../public/navicons/search.svg';
+import Image from 'next/image';
+import ContactIcon from '../public/navicons/contact-icon.svg';
+import DownIconTopNav from '../public/navicons/down-icon-topnav.svg';
 
 const TopNav = () => {
     return (
         <div className='flex bg-white justify-between mb-2 py-[12px] px-[32px]'>
-            <div className='flex gap-2'>
-                <h1>Payments</h1>
-                <p>?</p>
-                <p>How it works</p>
+            <div className='flex gap-2 items-center'>
+                <h1 className='text-[#1A181E] font-normal text-[15px]'>Payments</h1>
+                <Image src={HelpIcon} alt="Help Icon" width={14} height={14} />
+                <p className='text-[#4D4D4D] font-normal text-xs'>How it works</p>
             </div>
-            <input type="text" placeholder="Search features,tutorials,etc." className="border border-gray-300 rounded-md px-2 py-1 w-[200px] mr-2" />
-            <div className="flex gap-1">
-                <div className="rounded-xl bg-[#94a3b8] text-white px-4 py-1 rounded-md"></div>
-                <div className="rounded-xl bg-[#94a3b8] text-white px-4 py-1 rounded-md"></div>
+            <div className='flex bg-[#F2F2F2] items-center gap-x-2 rounded-md w-[400px] px-[16px] py-[9px]'>
+                <Image src={SearchIcon} alt="Search Icon" width={14} height={14} />
+                <input type="text" placeholder="Search features,tutorials,etc..." className="text-[#808080] w-full bg-[#F2F2F2] focus:border-none overflow-hidden outline-none whitespace-nowrap" />
+            </div>
+            <div className="flex gap-1 mx-5">
+                <div className="rounded-3xl bg-[#E6E6E6] text-white px-2.5 rounded-md">
+                    <Image src={ContactIcon} alt="Contact Icon" className='py-2 px-0.5 w-[20px] h-full' />
+                </div>
+                <div className="rounded-3xl bg-[#E6E6E6] text-white px-2.5 rounded-md">
+                    <Image src={DownIconTopNav} alt="Down Icon Top Nav" className='py-2 px-0.5 w-[20px] h-full' />
+                </div>
             </div>
         </div>
     )
