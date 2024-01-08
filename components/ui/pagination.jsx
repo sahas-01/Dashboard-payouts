21fbd78bda20a38b36496e3f7b7eaf5009b919b5
Sahas <sahasvivek001@gmail.com>
2024-01-09 00:34:09 +0530

    fix: pagination as per designs

--- components/ui/pagination.jsx
+++ components/ui/pagination.jsx
@@ -83,7 +83,7 @@ const PaginationEllipsis = ({
     aria-hidden
     className={cn("flex h-9 w-9 items-center justify-center", className)}
     {...props}>
-    <MoreHorizontal className="h-4 w-4" />
+    <MoreHorizontal className="h-4 w-4 mt-3" />
     <span className="sr-only">More pages</span>
   </span>
 )
--- pages/index.js
+++ pages/index.js
@@ -25,50 +25,50 @@ const invoices = [
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
   {
     orderId: "#281209",
     orderDate: "7 July,2023",
-    totalAmount: "1278.23",
-    transactionFees: "22",
+    totalAmount: "₹1,278.23",
+    transactionFees: "₹22",
   },
 ]
 
@@ -81,7 +81,7 @@ export default function Home() {
         <TopNav />
         <div className="mx-10">
           <h1 className="font-semibold text-lg my-4">Overview</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <HomeCard
               heading="Online orders"
               text="231"
@@ -93,24 +93,26 @@ export default function Home() {
           </div>
           <h1 className="font-semibold text-lg my-4">Transactions | This month</h1>
           <div className="bg-white w-full">
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between py-5 px-2">
               <input
                 type="text"
                 placeholder="Search by order ID.."
                 className="border border-gray-300 rounded-md px-2 py-1 w-[200px] mr-2"
               />
               <div className="flex gap-2">
-                <button className="bg-[#06b6d4] text-white px-4 py-1 rounded-md">Sort</button>
-                <button className="bg-[#06b6d4] text-white px-4 py-1 rounded-md">Download</button>
+                <button className="bg-white text-[#4D4D4D] border-[#D9D9D9] border-2 px-4 py-1 rounded-md">Sort</button>
+                <button className="bg-white text-[#4D4D4D] border-[#D9D9D9] border-2 px-4 py-1 rounded-md">Download</button>
               </div>
             </div>
             <Table>
               <TableHeader>
                 <TableRow className="bg-[#F2F2F2]">
-                  <TableHead className="w-[100px] text-black">Order ID</TableHead>
-                  <TableHead className="text-black">Order Date</TableHead>
-                  <TableHead className="text-black">Order amount</TableHead>
-                  <TableHead className="text-right text-black">Transaction Fees</TableHead>
+                  <TableHead className="w-[100px] text-[#4D4D4D] font-medium text-sm">Order ID</TableHead>
+                  <TableHead className="w-[100px] text-[#4D4D4D] font-medium text-sm">
+                    Order Date
+                  </TableHead>
+                  <TableHead className="w-[100px] text-right text-[#4D4D4D] font-medium text-sm">Order amount</TableHead>
+                  <TableHead className="w-[100px] text-right text-[#4D4D4D] font-medium text-sm">Transaction Fees</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
@@ -118,33 +120,41 @@ export default function Home() {
                   <TableRow key={invoice.invoice}>
                     <TableCell className="font-medium text-[#146EB4]">{invoice.orderId}</TableCell>
                     <TableCell>{invoice.orderDate}</TableCell>
-                    <TableCell>{invoice.totalAmount}</TableCell>
+                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>
                     <TableCell className="text-right">{invoice.transactionFees}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
-            <Pagination>
+            <Pagination className='pt-3'>
               <PaginationContent>
                 <PaginationItem>
-                  <PaginationPrevious href="#" />
+                  <PaginationPrevious className='text-[#4D4D4D] border-[1px] border-[#D9D9D9] text-sm font-normal' href="#" />
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationLink href="#">1</PaginationLink>
+                  <PaginationLink className='text-[#4D4D4D] text-sm font-normal' href="#">1</PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationLink className='bg-cyan-500 text-white' href="#" isActive>
-                    2
-                  </PaginationLink>
+                  <PaginationEllipsis className='text-[#4D4D4D] text-sm font-normal' />
+                </PaginationItem>
+                {/* <PaginationItem>
+                  <PaginationLink href="#">10</PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationLink href="#">3</PaginationLink>
-                </PaginationItem>
+                  <PaginationLink href="#">11</PaginationLink>
+                </PaginationItem> */}
+                {
+                  [10,11,12,13,14,15,16,17,18].map((item,index) => (
+                    <PaginationItem key={index}>
+                      {/* in the first item, the background color is supposed to be blue */}
+                      <PaginationLink className={
+                        index === 0 ? 'text-white text-sm font-medium rounded-md bg-[#146EB4]' : 'text-[#4D4D4D] text-sm font-normal'
+                      } href="#">{item}</PaginationLink>
+                    </PaginationItem>
+                  ))
+                }
                 <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationNext href="#" />
+                  <PaginationNext className='text-[#4D4D4D] border-[1px] border-[#D9D9D9] text-sm font-normal' href="#" />
                 </PaginationItem>
               </PaginationContent>
             </Pagination>
