a5570fec095a2ea2e8b0e182a4b097ba61a91322
Sahas <sahasvivek001@gmail.com>
2024-01-08 19:26:39 +0530

    feat: shadcn ui setup

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout"
+
+
+export default function Custom404() {
+    return (
+
+        <Layout pageTitle="404 - Page Not Found">
+            <p>Oops! The page you are looking for is not found!</p>
+        </Layout>
+    );
+}
